8d0ef55045fb9d0ea64d93c1bc733acef7cf0d8e
Aashish12-S <tarbundiyaaashish918@gmail.com>
2025-12-31 08:43:39 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,86 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#assignment operator
+x=10
+print(x)
+10
+#uranary minus
+a=2
+print(-a)
+-2
+#relational
+a=10
+b=6
+print(a>b)
+True
+print(a==b)
+False
+
+#logical
+a=true
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    a=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+
+
+#logical
+a=True
+b=False
+print(a and b)
+False
+print(a or b)
+True
+print(not a)
+False
+
+#boolean
+is pass=True
+SyntaxError: invalid syntax
+
+#boolean
+is_pass=True
+print(is_pass)
+True
+
+#bitwise
+a=5
+b=4
+print(a&b)
+4
+print(a)
+5
+print(a|b)
+5
+#membership
+numbers=[1,2,3,4,5,6]
+print(x is y)
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    print(x is y)
+NameError: name 'y' is not defined
+print(xisy)
+Traceback (most recent call last):
+  File "<pyshell#39>", line 1, in <module>
+    print(xisy)
+NameError: name 'xisy' is not defined
+#membership
+numbers=[1,2,3,4,5,6]
+print(2 in numbers)
+True
+print(5 not in numbers)
+False
+
+#identify
+a=10
+b=10
+print(a is b)
+True
+print(a is not b)
+False
+ #arithmetic
+
+a=5
+b=4
+print(a+b)
+9
